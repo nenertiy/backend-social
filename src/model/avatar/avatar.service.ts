26e2780db3f67e4efbea9346259b663788cad203
nenertiy <nenertiy@gmail.com>
2025-03-03 09:18:50 +0300

    fix: upload avatar for user

--- src/model/avatar/avatar.service.ts
+++ src/model/avatar/avatar.service.ts
@@ -11,8 +11,6 @@ import {
   PutObjectCommand,
   DeleteObjectCommand,
 } from '@aws-sdk/client-s3';
-// import { InjectS3, S3 } from 'nestjs-s3';
-// import { DeleteObjectCommand } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class AvatarService implements OnModuleInit {
@@ -20,7 +18,6 @@ export class AvatarService implements OnModuleInit {
   private s3: S3Client;
 
   constructor(
-    // @InjectS3() private readonly s3: S3,
     private readonly configService: ConfigService,
     private readonly avatarRepository: AvatarRepository,
   ) {}
@@ -39,44 +36,7 @@ export class AvatarService implements OnModuleInit {
     });
   }
 
-  // async onModuleInit() {
-  //   this.bucketName = this.configService.get<string>('S3_BUCKET_NAME');
-  //   console.log('Bucket name:', this.bucketName);
-
-  //   if (!this.bucketName) {
-  //     throw new Error('S3_BUCKET_NAME is not defined in environment variables');
-  //   }
-  // }
-
-  // async createAvatar(userId: string, file: Express.Multer.File) {
-  //   const filename = `avatar/${userId}-avatar.png`;
-
-  //   await this.avatarRepository.delete(userId);
-
-  //   try {
-  //     await this.s3.putObject({
-  //       Bucket: this.bucketName,
-  //       Key: filename,
-  //       Body: file.buffer,
-  //       ContentType: file.mimetype,
-  //     });
-  //   } catch (error) {
-  //     throw new InternalServerErrorException('Failed to upload avatar to S3');
-  //   }
-
-  //   const url = `${this.configService.get('S3_ENDPOINT')}/${this.bucketName}/${filename}`;
-
-  //   return this.avatarRepository.create(userId, url, filename);
-  // }
-
   async createAvatar(userId: string, file: Express.Multer.File) {
-    console.log('Uploading file:', {
-      originalName: file.originalname,
-      mimetype: file.mimetype,
-      size: file.size,
-      bufferExists: !!file.buffer,
-    });
-
     if (!file.buffer) {
       throw new BadRequestException('File buffer is missing');
     }
@@ -98,6 +58,7 @@ export class AvatarService implements OnModuleInit {
     }
 
     const url = `${this.configService.get('S3_ENDPOINT')}/${this.bucketName}/${filename}`;
+
     return this.avatarRepository.create(userId, url, filename);
   }
 
