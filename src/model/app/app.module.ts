50151bf93dc85cda08afcb9a10365c7a6e52d888
nenertiy <nenertiy@gmail.com>
2025-03-03 16:44:23 +0300

    fix and refactor app module

--- src/model/app/app.module.ts
+++ src/model/app/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { S3Module } from 'nestjs-s3';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
 import { TokenModule } from '../token/token.module';
@@ -13,23 +12,6 @@ import config from 'src/config/config';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
-    S3Module.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        config: {
-          credentials: {
-            accessKeyId: configService.get('S3_ACCESS_KEY'),
-            secretAccessKey: configService.get('S3_SECRET_KEY'),
-          },
-          endpoint: configService.get('S3_ENDPOINT'),
-          region: configService.get('S3_REGION'),
-          forcePathStyle: true,
-          s3ForcePathStyle: true,
-          signatureVersion: 'v4',
-        },
-      }),
-    }),
     UserModule,
     AvatarModule,
     AuthModule,
