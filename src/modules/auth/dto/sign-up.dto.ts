2e09dc4d3415578c971f5e9b7e9cce09bd80f7fa
nenertiy <nenertiy@gmail.com>
2025-03-21 22:55:53 +0300

    fix imports

--- src/modules/auth/dto/sign-up.dto.ts
+++ src/modules/auth/dto/sign-up.dto.ts
@@ -1,3 +1,3 @@
-import { CreateUserDto } from 'src/model/user/dto/create-user.dto';
+import { CreateUserDto } from 'src/modules/user/dto/create-user.dto';
 
 export class SignUpDto extends CreateUserDto {}
--- src/modules/token/strategies/jwt.strategy.ts
+++ src/modules/token/strategies/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { UserService } from 'src/model/user/user.service';
+import { UserService } from 'src/modules/user/user.service';
 import { JwtPayload } from 'src/common/types/jwt-payload';
 
 @Injectable()
